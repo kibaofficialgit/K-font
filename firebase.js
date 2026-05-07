76917651b757c6f55ac24c3fb1070145c55c1b64
kibaofficialgit <kibalvl133@gmail.com>
2026-05-07 22:12:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, setDoc, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, addDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAVM9g4OTyXZYB6vrJZCsDad-4yllHck3I",
   authDomain: "kfont-194c6.firebaseapp.com",
   projectId: "kfont-194c6",
-  storageBucket: "kfont-194c6.firebasestorage.app",
+  storageBucket: "kfont-194c6.appspot.com",
   messagingSenderId: "434182253027",
   appId: "1:434182253027:web:0befaca151328811d298d6"
 };
@@ -13,8 +13,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// make global
 window.db = db;
-window.setDoc = setDoc;
-window.doc = doc;
-window.getDoc = getDoc;
+window.addDoc = addDoc;
+window.collection = collection;
+window.getDocs = getDocs;
